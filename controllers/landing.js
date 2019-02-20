682379ce83d29bc1cfde4c4f2c8ebf84391fbf9b
jeffau13 <jeffau13@gmail.com>
2019-02-19 22:01:27 -0500

    saves leads into DB

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,7 +1,15 @@
-exports.get_landing = function(req, res, next) {
+const models = require('../models');
+
+exports.get_landing = function (req, res, next) {
   res.render('landing', { title: 'Express' });
 };
-exports.submit_lead = function(req, res, next) {
-  console.log('lead email', req.body.lead_email);
-  res.redirect('/');
+exports.submit_lead = function (req, res, next) {
+  // console.log('lead email', req.body.lead_email);
+
+  return models.Lead.create({
+    email: req.body.lead_email
+  }).then(lead => {
+    res.redirect('/');
+  })
+
 };
