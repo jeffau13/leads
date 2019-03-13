376a62fc52e9dcd0ffe2d995164e8934f1b405cb
jeffau13 <jeffau13@gmail.com>
2019-03-13 15:14:59 -0400

    migrated all pages to bootstrap and created separate leads.pug for viewing list of leads

--- controllers/landing.js
+++ controllers/landing.js
@@ -16,7 +16,7 @@ exports.submit_lead = function (req, res, next) {
 //renders list of lead
 exports.show_leads = function (req, res, next) {
   return models.Lead.findAll().then(leads =>{
-    res.render('landing', { title: 'Express' , leads: leads});
+    res.render('lead/leads', { title: 'Express' , leads: leads});
   })
 };
 //render individual lead from database query:
@@ -26,7 +26,7 @@ exports.show_lead = function (req, res, next) {
       id : req.params.lead_id
     }
   }).then(lead => {
-    res.render('lead',{lead:lead});
+    res.render('lead/lead',{lead:lead}); 
   })  
 };
 
