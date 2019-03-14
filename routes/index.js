419a34f4eeed593e87ff15272b2970505bdd6276
jeffau13 <jeffau13@gmail.com>
2019-03-14 11:31:15 -0400

    implemented get route for login and signup

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 let landing = require('../controllers/landing');
+let user = require('../controllers/user');
+
 
 // get home page
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
+
+//get login/signup
+router.get('/login', user.show_login);
+router.get('/signup',user.show_signup);
+
 router.get('/leads', landing.show_leads);
 router.get('/lead/:lead_id', landing.show_lead);
 //shows form for editing lead:
