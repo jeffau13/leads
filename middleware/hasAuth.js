7029812997a018694d7dc625b1b2aef7c0b9d312
jeffau13 <jeffau13@gmail.com>
2019-03-14 18:45:30 -0400

    implemented middleware to check for admin access control. Lead routes are protected by hasAuth. Only admin has access to that

--- middleware/hasAuth.js
+++ middleware/hasAuth.js
@@ -6,3 +6,10 @@ exports.isLoggedIn = function (req,res,next){
     else
         next(createError(403,"Must be logged in to view this page"));
 }
+
+exports.hasAuth = function(req,res,next){
+    if(req.user && req.user.is_admin == true)
+        next();
+    else
+        next(createError(403,"Unauthorized"));
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 let landing = require('../controllers/landing');
 let user = require('../controllers/user');
 
-let {isLoggedIn} = require('../middleware/hasAuth.js');
+let {isLoggedIn,hasAuth} = require('../middleware/hasAuth.js');
 
 // get home page
 router.get('/', landing.get_landing);
@@ -20,12 +20,12 @@ router.post('/logout',user.logout);
 router.get('/logout', user.logout);
 
 
-router.get('/leads', isLoggedIn, landing.show_leads);
-router.get('/lead/:lead_id', landing.show_lead);
+router.get('/leads', hasAuth, landing.show_leads);
+router.get('/lead/:lead_id', hasAuth, landing.show_lead);
 //shows form for editing lead:
-router.get('/lead/:lead_id/edit', landing.show_edit_lead);
+router.get('/lead/:lead_id/edit', hasAuth, landing.show_edit_lead);
 //submits form of lead edit:
-router.post('/lead/:lead_id/edit', landing.edit_lead);
-router.post('/lead/:lead_id/delete', landing.delete_lead);
-router.post('/lead/:lead_id/delete-json', landing.delete_lead_json);
+router.post('/lead/:lead_id/edit', hasAuth, landing.edit_lead);
+router.post('/lead/:lead_id/delete', hasAuth, landing.delete_lead);
+router.post('/lead/:lead_id/delete-json', hasAuth, landing.delete_lead_json);
 module.exports = router;
